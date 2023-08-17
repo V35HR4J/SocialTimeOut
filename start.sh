6acf5eaaab976ff859785b2a9a8a3512b2c3309b
Suyogya Khanal <suyogkhanal4@gmail.com>
2023-08-17 23:57:05 +0530

    Instead of hard-coding blacklisted urls in a file, put them in a file

--- start.sh
+++ start.sh
@@ -3,23 +3,14 @@
 # Create a backup of /etc/hosts
 sudo cp /etc/hosts /etc/hosts.backup
 
-# List of social media URLs
-social_media_urls=(
-    "www.facebook.com"
-    "www.instagram.com"
-    "www.pinterest.com"
-    "www.snapchat.com"
-    "www.tiktok.com"
-    "www.reddit.com"
-    "www.tumblr.com"
-    "www.whatsapp.com"
-    "www.wechat.com"
-    "www.viber.com"
-    "www.messenger.com"
-    "www.quora.com"
-    "www.reddit.com"
-    "www.twitch.tv"
-)
+
+filename="blacklist.txt"
+declare -a social_media_urls
+
+while IFS= read -r line; do
+    social_media_urls+=("$line")
+done < "$filename"
+
 
 # Append the URLs to /etc/hosts
 for url in "${social_media_urls[@]}"; do
